0087d50675827a8597ac56c5d58270ffd16be1ac
CiVil75 <78097795+CiVil75@users.noreply.github.com>
2026-06-23 11:19:22 +0200

    Fix CO2 intensity indicator reference

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -17,7 +17,7 @@ INDICATORS = {
     "Population": ("SP.POP.TOTL", "MPax", 1e6),
     "GDP per capita": ("NY.GDP.PCAP.PP.CD", "k$/pax", 1e3),
     "Energy intensity": ("EG.GDP.PUSE.KO.PP", "Wh/$", 1),
-    "CO2 intensity": ("EN.GHG.CO2.RT.GDP.PP", "kgCO2/$", 1),
+    "CO2 intensity": ("EN.GHG.CO2.RT.GDP.PP.KO", "kgCO2/$", 1),
     "CO2 emissions": ("EN.GHG.ALL.LU.MT.CE.AR5", "GtCO2/y", 1e9)
 }
 
